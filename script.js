52981624908cfab5cd268f3237134708dbe55ab0
baliharsha <baliharsha75@gmail.com>
2024-02-21 14:56:50 +0530

    responsive

---
+++ script.js
@@ -0,0 +1,99 @@
+let btn= document.querySelector(".p6btn")
+console.log(btn);
+
+
+
+    
+    
+    btn.addEventListener("mouseenter", function(){
+    
+   btn.classList.add("hover")
+   setTimeout(()=>{
+    btn.classList.remove("hover")
+   },2000)
+
+
+
+})
+
+btn.addEventListener("mouseleave", function(){
+
+    btn.classList.remove("hover")
+
+
+})
+
+let navbtn= document.querySelector(".navbtn")
+console.log(btn);
+
+
+
+    
+    
+    navbtn.addEventListener("mouseenter", function(){
+    
+   navbtn.classList.add("hover")
+   setTimeout(()=>{
+    btn.classList.remove("hover")
+   },2000)
+
+
+
+})
+
+navbtn.addEventListener("mouseleave", function(){
+
+    navbtn.classList.remove("hover")
+
+
+})
+
+let pagebtn= document.querySelector(".p4btn")
+console.log(btn);
+
+
+
+    
+    
+    pagebtn.addEventListener("mouseenter", function(){
+    
+   pagebtn.classList.add("hover")
+   setTimeout(()=>{
+    pagebtn.classList.remove("hover")
+   },2000)
+
+
+
+})
+
+pagebtn.addEventListener("mouseleave", function(){
+
+    pagebtn.classList.remove("hover")
+
+
+})
+
+let page2btn= document.querySelector(".page2btn")
+console.log(btn);
+
+
+
+    
+    
+    page2btn.addEventListener("mouseenter", function(){
+    
+   page2btn.classList.add("hover")
+   setTimeout(()=>{
+    page2btn.classList.remove("hover")
+   },2000)
+
+
+
+})
+
+page2btn.addEventListener("mouseleave", function(){
+
+    page2btn.classList.remove("hover")
+
+
+})
